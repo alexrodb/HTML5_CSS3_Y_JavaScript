a4fc9a82aff5cf597fd654413e6ffe794b065b44
Alexander Rodríguez Benavides <alerod1991@gmail.com>
2021-04-24 16:01:52 -0500

    Ciclos o loops
    Ciclos o loops

---
+++ JavaScript/6 - Ciclos o loops/3 - Ciclo DO...WHILE/js/app.js
@@ -0,0 +1,14 @@
+"use strict"
+
+//********************************0
+//*** Ciclo DO..WHILE | Ciclo Indefinido
+// Iteración indeterminada o desconocida
+
+var productos = 10; 
+
+do { 
+	console.log( 'Producto vendido o enviado');
+	productos--;
+	debugger;
+} 
+while(productos>=1)
